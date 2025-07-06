691e5d102b04fd96929e97dddf2c5e20ac8de9d7
kurtishvili <kurtishvilii@gmail.com>
2025-07-06 15:08:45 +0400

    test

---
+++ Algorithms/In.cs
@@ -0,0 +1,16 @@
+namespace Algorithms;
+
+public class In
+{
+    public static IEnumerable<int> ReadInts(string filePath)
+    {
+        using (TextReader reader = File.OpenText(filePath))
+        {
+            string? lastLine;
+            while ((lastLine = reader.ReadLine()) != null)
+            {
+                yield return int.Parse(lastLine);
+            }
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using Algorithms;
+using Algoritihms_DataStruct_Lib;
+
+var ints = In.ReadInts("1kints.txt").ToArray();
+
+var watch = new Stopwatch();
+watch.Start();
+
+var triplets = ThreeSum.Count(ints);
+
+watch.Stop();
+
+Console.WriteLine($"the number of triplets {triplets}");
+Console.WriteLine($"time taken {watch.Elapsed:g}");
+
+Console.Read();
